67b10acd3c147d3fb24e02b980cfbc043f0f7927
siddik1 <siddik@navadhiti.com>
2021-01-07 13:22:45 +0530

    table added in getinvolved and some filenames changed

---
+++ js/jobList.js
@@ -0,0 +1,16 @@
+"use strict";
+
+function renderJob(job){
+    console.log(job)
+    job.forEach((joblist)=>{
+        jobname.innerText += joblist.jobTitle + "\n\n";
+        jobid.innerText += joblist.employeeCode + "\n\n";
+//        console.log(job.jobTitle);
+    })
+}
+
+fetch("./data.json")
+    .then(result => result.json())
+    .then(data=>
+        renderJob(data.jobs)
+    );
