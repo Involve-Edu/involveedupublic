2b3f1a4f88eede9084b4e030285d479de716b659
sarath15 <sarath@navadhiti.com>
2021-01-08 11:59:23 +0530

    json data condition check

--- js/jobList.js
+++ js/jobList.js
@@ -1,26 +1,32 @@
 "use strict";
 
 function renderJob(job) {
-     console.log(job); //this displays the json
+    console.log(job); //this displays the json
+    console.log(job.length)
+    if (job.length < 1) {
+        document.getElementById("check").style.display="none"
+        document.getElementById("job").style.display="block"
+        console.log("no jobs")
 
-     var jobtable = document.getElementById('tablejob');
-         /*console.log(jobtable);*/
-         jobtable.className = 'table table-striped';
+    } else {
+        document.getElementById("check").style.display="block"
+        var jobtable = document.getElementById('tablejob');
+        /*console.log(jobtable);*/
+        jobtable.className = 'table table-striped';
 
-     var jobtablehead1 = document.createElement('th');
-     jobtablehead1.innerText = 'Job Title';
-     var jobtablehead2 = document.createElement('th');
-            jobtablehead2.innerText = 'Description';
+        var jobtablehead1 = document.createElement('th');
+        jobtablehead1.innerText = 'Job Title';
+        var jobtablehead2 = document.createElement('th');
+        jobtablehead2.innerText = 'Description';
 
-            jobtable.append(jobtablehead1, jobtablehead2);
+        jobtable.append(jobtablehead1, jobtablehead2);
 
-     for (var i = 0; i < job.length; i++) {
-         var Result = job[i];
-         /*console.log(Object.keys(Result));
-         console.log(Object.values(Result));*/
+        for (var i = 0; i < job.length; i++) {
+            var Result = job[i];
+            /*console.log(Object.keys(Result));
+            console.log(Object.values(Result));*/
 
-         /*if( job.length >0 ){*/
-
+            /*if( job.length >0 ){*/
 
 
             let jobtablerow = document.createElement('tr');
@@ -32,21 +38,21 @@ function renderJob(job) {
             jobtabledata2.innerText += Result.employeeCode;
 
             jobtable.append(jobtablerow, jobtabledata1, jobtabledata2);
-       /* }
-        else {
-        let jobtableerror = document.createElement('p');
-        jobtableerror.innerText = 'There are no jobs available';
-        jobtable.append(jobtableerror);
-        }*/
-     }
+            /* }
+             else {
+             let jobtableerror = document.createElement('p');
+             jobtableerror.innerText = 'There are no jobs available';
+             jobtable.append(jobtableerror);
+             }*/
+        }
+    }
 
-  }
 
-
+}
 
 
 fetch("./data.json")
     .then(result => result.json())
-    .then(data=>
+    .then(data =>
         renderJob(data.jobs)
     );
